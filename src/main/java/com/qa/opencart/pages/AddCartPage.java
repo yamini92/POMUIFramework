eeac8f29442c5fa1de0612063381bf8ca353aa05
yamini92 <kapadia.yamini@gmail.com>
2023-01-05 12:48:56 +0530

    Updated code as per review comment

--- src/main/java/com/qa/opencart/pages/AddCartPage.java
+++ src/main/java/com/qa/opencart/pages/AddCartPage.java
@@ -1,5 +1,12 @@
 package com.qa.opencart.pages;
 
-public class AddCartPage {
+import org.openqa.selenium.By;
 
+public class AddCartPage {
+	
+	By shoppiingCartHeader = By.cssSelector("div#content h1");
+
+	public void click() {
+		System.out.println("Clicked ");
+	}
 }
